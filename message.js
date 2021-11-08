2a82d6854807b9f9ac4674eb63e0180571d2f108
Miguel Chavez <miguel.chavez65@gmail.com>
2021-11-07 18:00:51 -0700

    first draft complete

--- message.js
+++ message.js
@@ -8,8 +8,28 @@ function createMessage(){
         theAction: ['have a great time', 'do terrific things', 'achieve your goals', 'accomplish everything you set yourself to', 'come across new opportunities'],
         goodBye: ['Love you', 'Much love', '~Blessings~', 'Take care', 'See you around']
     }
+    let finalMessage = [];
+    let randomSelect = arrayLength => {
+        return Math.floor(Math.random() * arrayLength);
+    }
 
-    
+    for(let element in message){
+        let randIndex = randomSelect(message[element].length);
+        switch(element){
+            case 'time':
+                finalMessage.push(message[element][randIndex]);
+                break;
+            case 'theAction':
+                finalMessage.push(`, you are going to ${message[element][randIndex]}`);
+                break;
+            case 'goodBye':
+                finalMessage.push(` ${message[element][randIndex]}`);
+                break;
+        }
+    }
+    return finalMessage;
 
 
 }
+
+console.log(createMessage());
