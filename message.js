5f5ee54c298b8163686f8873f521f80072374e63
Miguel Chavez <miguel.chavez65@gmail.com>
2021-11-07 18:03:59 -0700

    fixed some punctuation issues

--- message.js
+++ message.js
@@ -4,8 +4,8 @@
 
 function createMessage(){
     const message = {
-        time: ['Today', 'This week', 'Tomorrow', 'This year', 'This month'],
-        theAction: ['have a great time', 'do terrific things', 'achieve your goals', 'accomplish everything you set yourself to', 'come across new opportunities'],
+        time: ['Today,', 'This week,', 'Tomorrow,', 'This year,', 'This month,'],
+        theAction: ['have a great time.', 'do terrific things.', 'achieve your goals.', 'accomplish everything you set yourself to.', 'come across new opportunities.'],
         goodBye: ['Love you', 'Much love', '~Blessings~', 'Take care', 'See you around']
     }
     let finalMessage = [];
@@ -20,14 +20,14 @@ function createMessage(){
                 finalMessage.push(message[element][randIndex]);
                 break;
             case 'theAction':
-                finalMessage.push(`, you are going to ${message[element][randIndex]}`);
+                finalMessage.push(`you are going to ${message[element][randIndex]}`);
                 break;
             case 'goodBye':
-                finalMessage.push(` ${message[element][randIndex]}`);
+                finalMessage.push(`${message[element][randIndex]}`);
                 break;
         }
     }
-    return finalMessage;
+    return finalMessage.join(' ');
 
 
 }
