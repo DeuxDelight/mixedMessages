73e44593db74604c3556b84a0aeb5a0c491a06bc
Miguel Chavez <miguel.chavez65@gmail.com>
2021-11-07 17:52:31 -0700

    added messages to choose from

--- message.js
+++ message.js
@@ -1,3 +1,15 @@
-const message = {
+//
+
+
+
+function createMessage(){
+    const message = {
+        time: ['Today', 'This week', 'Tomorrow', 'This year', 'This month'],
+        theAction: ['have a great time', 'do terrific things', 'achieve your goals', 'accomplish everything you set yourself to', 'come across new opportunities'],
+        goodBye: ['Love you', 'Much love', '~Blessings~', 'Take care', 'See you around']
+    }
+
     
+
+
 }
